e83fe5e80b6a48211adb241ecfd96cc6aa0eab45
Asher Stoppard <asher@asherstoppard.com>
2020-10-20 00:22:53 +0100

    feature/add-consultancy-projects - Add placeholder with notes

---
+++ components/Consultancy/index.js
@@ -0,0 +1,11 @@
+const Consultancy = () => (
+  <ul>
+    <li>ModelDrive</li>
+    <li>LiftsTo</li>
+    <li>Staircase Renovation</li>
+    <li>Miles and Lincoln</li>
+    <li>WPHP</li>
+    <li>CLHQ</li>
+    <li>The Frustrated Chef</li>
+  </ul>
+)
