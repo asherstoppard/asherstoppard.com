618cb20e49a6b026009b12598187b5f11594c1b9
Asher Stoppard <asher@asherstoppard.com>
2020-10-24 18:24:15 +0100

    fix/html-lang - Fix indentation

--- components/Head/index.js
+++ components/Head/index.js
@@ -3,17 +3,17 @@ import Head from 'next/head'
 
 const HeadComponent = ({ title, description }) => (
   <Head>
-      <html lang="en" />
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
-        rel="stylesheet"
-      />
+    <html lang="en" />
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
+      rel="stylesheet"
+    />
   </Head>
 )
 
