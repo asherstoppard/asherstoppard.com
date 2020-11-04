b5398a1410e79453f14f76a1727dbd1b41f99cd9
Asher Stoppard <asher@asherstoppard.com>
2020-11-04 00:21:52 +0000

    feature/improve-custom-document - Add styled-component output to document for SSR

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <styles />
+        </Head>
         <body>
           <Main />
           <NextScript />
