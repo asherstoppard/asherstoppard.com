72f3a3c5cb9ca7cfb591136fa4f3a0fbd1965573
Asher Stoppard <asher@asherstoppard.com>
2020-11-04 00:47:10 +0000

    feature/change-timeline-colours - Change header colour and change indentation

--- components/Theme/index.js
+++ components/Theme/index.js
@@ -1,55 +1,56 @@
 import React from 'react'
 import T from 'prop-types'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
-import { normalize } from 'polished'
+import {ThemeProvider, createGlobalStyle} from 'styled-components'
+import {normalize} from 'polished'
 import variables from './variables'
 
 const Globals = createGlobalStyle`
-	${normalize()};
-	
-	*,
-	*:before,
-	*:after {
-		box-sizing: border-box;
-	}
-
-	body {
-	  font: 300 1rem 'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
-		color: #263238;
-	}
-
-	h1,
-	h2,
-	h3,
-	h4,
-	h5,
-	h6 {
-	  font-weight: 300;
-	  font-size: 3rem;
-	}
-
-	strong,
-	b {
-		font-weight: 700;
-	}
-	
-	img {
-		max-width: 100%;
-		vertical-align: middle;
-	}
-	
-	a {
-		position: relative;
-		text-decoration: none;
-		color: ${({ theme }) => theme.colors.brandPrimary};
-		font-weight: 600;
-	}
+  ${normalize()};
+  
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+  
+  body {
+    font: 300 1rem 'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+    color: #263238;
+  }
+  
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 300;
+    font-size: 3rem;
+    color: #231557;
+  }
+  
+  strong,
+  b {
+    font-weight: 700;
+  }
+  
+  img {
+    max-width: 100%;
+    vertical-align: middle;
+  }
+  
+  a {
+    position: relative;
+    text-decoration: none;
+    color: ${({theme}) => theme.colors.brandPrimary};
+    font-weight: 600;
+  }
 `
 
-const Theme = ({ children }) => (
+const Theme = ({children}) => (
   <ThemeProvider theme={variables}>
     <>
-      <Globals />
+      <Globals/>
       {children}
     </>
   </ThemeProvider>
