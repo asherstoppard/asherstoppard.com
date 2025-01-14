c8f461fe9841ef10ca9ac3ab9be030061bedf6e9
Asher Stoppard <asher@asherstoppard.com>
2025-01-14 21:43:16 +0000

    feat(layout): amend typography imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
 import { PropsWithChildren } from 'react'
-import { Metadata } from 'next'
-import { Outfit } from 'next/font/google'
 import { StyledComponentsRegistry } from '@/lib'
 import { MainLayout } from '@/layouts'
 
-const outfitFont = Outfit({
-  weight: ['200', '300', '400', '600', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-export const metadata: Metadata = {
-  title: 'Asher Stoppard - Web App Consultant specialising in Javascript',
-  description:
-    'A software engineering consultant specialising in isomorphic javascript applications, centralisation and componentisation, architecture, CI/CD and testing.',
-}
-
 const RootLayout = ({ children }: PropsWithChildren) => (
   <html lang="en">
-    <body className={outfitFont.className}>
+    <head>
+      <title>Asher Stoppard - Web App Consultant specialising in Javascript</title>
+      <meta
+        name="description"
+        content="A software engineering consultant specialising in isomorphic javascript applications, centralisation and componentisation, architecture, CI/CD and testing."
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+    <body>
       <StyledComponentsRegistry>
         <MainLayout>{children}</MainLayout>
       </StyledComponentsRegistry>
